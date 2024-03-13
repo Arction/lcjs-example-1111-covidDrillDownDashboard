327766521c7bc8d5f58c0444d66af307193e9085
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-03-13 11:29:06 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const dashboardRows = 4
 const dashboard = lightningChart().Dashboard({
     numberOfColumns: 1,
     numberOfRows: dashboardRows,
-    theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,,
+    theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
 })
 const theme = dashboard.getTheme()
 
